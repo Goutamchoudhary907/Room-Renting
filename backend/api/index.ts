fdee33c1c852a0424e9c0caead3a8e71b3ffb60e
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-06-03 10:58:07 +0530

    update

---
+++ backend/api/index.ts
@@ -0,0 +1,4 @@
+import { configure } from '@vendia/serverless-express';
+import app from '../src/index';
+
+export default configure({ app });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -83,4 +83,4 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Not found' });
 });
 
-module.exports = app;
+export default app;
