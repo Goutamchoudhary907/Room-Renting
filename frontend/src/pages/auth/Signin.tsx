92987cdd8f2a26aa14496483ead4e6ef595ab457
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-07-06 09:03:15 +0530

    Fix OAuth error

--- frontend/src/pages/auth/Signin.tsx
+++ frontend/src/pages/auth/Signin.tsx
@@ -89,6 +89,8 @@ export const Signin =()=>{
                    onError={() => {
                      console.log("Google Login Failed");
                    }}
+                    useOneTap 
+                    ux_mode="redirect" 
                   />
               <button className="flex items-center border rounded bg-[#335ca6] justify-center text-white w-full h-10 md:h-[28px] lg:w-[320px] lg:mt-3 max-lg:h-10 max-lg:text-sm">
               <div className="w-5 h-5 md:w-6 md:h-6">
